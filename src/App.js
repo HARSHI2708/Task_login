971b440cf4ca62fddb07f7e8dd3355088fe573e1
HARSHI2708 <patilharshitha27@gmail.com>
2023-09-14 13:29:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import  Home  from "./Components/Home";
+import AssignTask from "./Components/AssignTask";
+import ViewTask from "./Components/ViewTask";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="container mt-5">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashboard" element={<Navbar />} />
+            <Route path="/assigntask" element={<AssignTask />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/viewtask" element={<ViewTask />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
